02eda23b0b952718ed3f89eb55f38669edceb1eb
Stefano Franchini <stefano.franchini77@gmail.com>
2020-12-09 15:01:40 +0100

    slider basic

--- js/script.js
+++ js/script.js
@@ -2,7 +2,31 @@ let app = new Vue (
     {
         el: "#root",
         data: {
-            
+            indexArray: 0,
+            images: [
+                "img/cava-bauxite.jpg",
+                "img/iceland.jpg",
+                "img/japan.jpg",
+                "img/smLeuca.jpg",
+                "img/norway.jpg",
+                "img/times-square.jpg",
+            ],
+        },
+        methods: {
+            prevImg: function () {
+                this.indexArray--;
+
+                if(this.indexArray < 0) {
+                    this.indexArray = this.images.length - 1;
+                }
+            },
+            nextImg: function () {
+                this.indexArray++;
+
+                if(this.indexArray == this.images.length) {
+                    this.indexArray = 0;
+                }
+            }
         }
     }
 );
